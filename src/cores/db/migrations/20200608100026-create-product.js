90bd34ea489277fba31e558a2a06bfd6829a47bf
Daniel <damledinh93.cs@gmail.com>
2020-06-08 17:13:56 +0700

    Update product model and seed

--- src/cores/db/migrations/20200608100026-create-product.js
+++ src/cores/db/migrations/20200608100026-create-product.js
@@ -9,15 +9,19 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       colour: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       branch: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       price: {
+        allowNull: false,
         type: Sequelize.DOUBLE
       },
       deletedAt: {
--- src/cores/db/seeders/20200608100026-demo-product.js
+++ src/cores/db/seeders/20200608100026-demo-product.js
@@ -1,11 +1,15 @@
-const hashPwd = require('../../commons/utils/hashPwd');
-
 module.exports = {
   up: async queryInterface => {
     const users = ['laptop', 'iphone', 'omo', 'Macbook'];
+    const colours = ['red', 'white', 'yellow', 'blue'];
+    const branch = ['Sony', 'Apple', 'obama', 'apple'];
+    const prices = [1000, 3400, 4000, 54200];
 
-    const payload = users.map(item => ({
+    const payload = users.map((item, index) => ({
       name: item,
+      colour: colours[index],
+      branch: branch[index],
+      price: prices[index],
       createdAt: new Date(),
       updatedAt: new Date(),
     }));
