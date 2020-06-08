0ad28482e343406238ada0f41bae3a11c9569d40
Daniel <damledinh93.cs@gmail.com>
2020-06-08 16:16:49 +0700

    Init libs

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    printWidth: 96,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'all',
+};
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    setupFiles: ['jest-plugin-unhandled-promise/setup', '<rootDir>/src/setupTests'],
+    setupFilesAfterEnv: ['jest-extended'],
+    coverageReporters: ['json-summary', 'lcov', 'text'],
+};
