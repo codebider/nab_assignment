4d164c8af780174358ce2fb99ffb473a336df922
Daniel <damledinh93.cs@gmail.com>
2020-06-08 18:17:48 +0700

    More unit test for query like

---
+++ src/cores/services/daos/utils.test.js
@@ -0,0 +1,30 @@
+const { queryLike } = require('./utils');
+
+describe('utils', () => {
+  describe('utils.queryLike function', () => {
+    it('should build correct query', () => {
+      // Give
+      const field = 'name';
+      const value = 'abc';
+      // When
+      const result = queryLike(field, value);
+      // Then
+      expect(result).toMatchInlineSnapshot(`
+        Object {
+          "name": Object {
+            Symbol(like): "%abc%",
+          },
+        }
+      `);
+    });
+
+      it('should build correct query when missing value', () => {
+          // Give
+          const field = 'name';
+          // When
+          const result = queryLike(field);
+          // Then
+          expect(result).toEqual({});
+      });
+  });
+});
