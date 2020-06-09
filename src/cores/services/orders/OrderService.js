6f11ed3c6903ba3ee083175d8fb0535f6bd6a91e
Daniel <damledinh93.cs@gmail.com>
2020-06-09 11:31:35 +0700

    Update order logic

--- src/cores/services/orders/OrderService.js
+++ src/cores/services/orders/OrderService.js
@@ -1,6 +1,7 @@
 const pick = require('lodash/pick');
 
 const throwIfMissing = require('../../../commons/assertion/throwIfMissing');
+const bigNumber = require('../../utils/math-utils');
 
 const REQUIRED_OPTIONS = ['logger', 'orderDao'];
 
@@ -25,6 +26,20 @@ class OrderService {
   async create(payload) {
     const { products, ...order } = payload;
     // TODO: Validate product ids
+
+    // Calculate total price
+    const { shippingFee } = order;
+    const totalPrice = bigNumber(shippingFee);
+
+    products.forEach(({ price, quantity }) => {
+      const productPrice = bigNumber(price)
+        .times(quantity)
+        .toNumber();
+
+      totalPrice.plus(productPrice);
+    });
+
+    Object.assign(order, { totalPrice: totalPrice.toFixed() });
 
     await this.orderDao.createOrder(order, products);
   }
